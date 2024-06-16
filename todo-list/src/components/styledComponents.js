5f42721a36dacaf1d0889b39c6200da1f70218a2
yminjuu <ymj0580@naver.com>
2024-06-16 17:52:02 +0900

    기본 골격 완성

---
+++ todo-list/src/components/styledComponents.js
@@ -0,0 +1,152 @@
+import styled from "styled-components";
+
+export const LoginWrapper = styled.div`
+  box-sizing: border-box;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  background: transparent;
+`;
+
+export const LogoWrapper = styled.div`
+  width: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: left;
+  padding: 60px;
+  margin-left: 60px;
+  margin-top: 60px;
+  background: transparent;
+  gap: 50px;
+`;
+
+export const InputWrapper = styled.div`
+  width: auto;
+  margin-right: 300px;
+  margin-bottom: 200px;
+  min-width: 400px;
+  display: flex;
+  flex-direction: column;
+  background: transparent;
+  align-items: flex-end;
+  justify-content: center;
+  gap: 15px;
+`;
+
+export const StyledForm = styled.form`
+  width: auto;
+  min-width: 200px;
+  background: transparent;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 15px;
+`;
+
+export const Logo = styled.img`
+  width: 320px;
+  height: 150px;
+  background: transparent;
+`;
+
+export const Description = styled.div`
+  width: 362px;
+  height: 40px;
+  flex-shrink: 0;
+  color: #fff;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  font-family: Pretendard;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  background: transparent;
+`;
+
+export const Input = styled.input`
+  width: 10rem;
+  height: 3rem;
+  flex-shrink: 0;
+  border-radius: 5px;
+  border: 2px solid #fff;
+  opacity: 0.3;
+  background: #fff;
+  box-shadow: 0px 4px 4px 0px #000;
+  text-align: center;
+  font-size: 15px;
+`;
+
+export const SubmitButton = styled.button`
+  width: 65px;
+  height: 31px;
+  flex-shrink: 0;
+  border-radius: 20px;
+  border: 2px solid #595656;
+  opacity: 0.3;
+  background: rgba(255, 255, 255, 0.8);
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.5);
+  text-align: center;
+  cursor: pointer;
+`;
+
+export const NotMem = styled.div`
+  width: auto;
+  color: #909090;
+  font-family: Pretendard;
+  font-size: 10px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  background: transparent;
+`;
+
+export const GoJoin = styled.div`
+  width: auto;
+  color: #000;
+  font-family: Pretendard;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 800;
+  line-height: normal;
+  text-decoration-line: underline;
+  background: transparent;
+  cursor: pointer;
+`;
+
+export const BaseContainer = styled.div`
+  border: 3px solid #fff;
+  box-shadow: 0px 4px 4px 0px rgba(81, 140, 255, 0.5);
+  background: transparent;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const BaseInnerContainer = styled.div`
+  background: #fff;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: left;
+`;
+
+export const Ellipse = styled.div`
+  width: 10px;
+  height: 10px;
+  flex-shrink: 0;
+  fill: rgba(0, 0, 0, 0.5);
+  filter: drop-shadow(0px 2px 4px rgba(0, 0, 0, 0.25));
+  background: transparent;
+`;
+
+export const GridLayout = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: repeat(2, auto);
+  gap: 20px;
+  width: auto;
+  height: auto;
+  background: transparent;
+`;
--- todo-list/src/pages/home.jsx
+++ todo-list/src/pages/home.jsx
@@ -1,20 +1,181 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
+import {
+  BaseContainer,
+  BaseInnerContainer,
+  Ellipse,
+  GridLayout,
+} from "../components/styledComponents";
 
 const Home = () => {
+  const LogoWrapper = styled.div`
+    width: auto;
+    height: auto;
+    background: transparent;
+    /* 로고: absolute */
+    position: absolute;
+    left: 30px;
+    top: 20px;
+  `;
+
+  const Logo = styled.img`
+    width: 180px;
+    height: 70px;
+    background: transparent;
+  `;
+
+  const ContentWrapper = styled.div`
+    width: 100%;
+    height: 100%;
+    background: transparent;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `;
+
+  const CalendarContainer = styled(BaseContainer)`
+    width: 367px;
+    height: 261px;
+    flex-shrink: 0;
+    grid-area: 1 / 1 / 2 / 2; /* 1행 1열 */
+  `;
+
+  const CalendarInnerContainer = styled(BaseInnerContainer)`
+    width: 355px;
+    height: 249px;
+    flex-shrink: 0;
+  `;
+
+  const TODOContainer = styled(BaseContainer)`
+    width: 665px;
+    height: 261px;
+    flex-shrink: 0;
+    grid-area: 1 / 2 / 2 / 4; /* 1행 2~3열 */
+  `;
+
+  const TODOInnerContainer = styled(BaseInnerContainer)`
+    width: 653px;
+    height: 249px;
+    flex-shrink: 0;
+  `;
+
+  const ListContainer = styled(BaseContainer)`
+    width: 1059px;
+    height: 261px;
+    flex-shrink: 0;
+    grid-area: 2 / 1 / 3 / 4; /* 2행 전체 */
+  `;
+
+  const ListInnerContainer = styled(BaseInnerContainer)`
+    width: 1047px;
+    height: 249px;
+    flex-shrink: 0;
+  `;
+
+  const ContainerTitle = styled.div`
+    margin-left: 30px;
+    margin-top: 5px;
+    width: 100%;
+    padding: 5px;
+    height: auto;
+    color: #000;
+    text-align: left;
+    font-family: Grandstander;
+    font-size: 1.5rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    background: transparent;
+  `;
+
+  const TODOInput = styled.textarea`
+    width: 90%;
+    height: 140px;
+    flex-shrink: 0;
+    border-radius: 7px;
+    border: 1px solid #c8c8c8;
+    background: #fff;
+    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+    padding: 10px;
+    font-size: 15px;
+    color: #b0b0b0;
+    font-family: Pretendard;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  `;
+
+  const ButtonWrapper = styled.div`
+    margin-right: 30px;
+    margin-bottom: 5px;
+    width: 100%;
+    padding: 5px;
+    height: auto;
+    background: transparent;
+    display: flex;
+    justify-content: end;
+  `;
+
+  const AddButton = styled.div`
+    border-radius: 10px;
+    border: 1px solid #7c7c7c;
+    background: #fff;
+    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+    cursor: pointer;
+    width: 35px;
+    height: 27px;
+    flex-shrink: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `;
+
+  const AddButtonImg = styled.img`
+    width: 20px;
+    height: 20px;
+    flex-shrink: 0;
+    background: transparent;
+  `;
+
   const { id } = useParams();
   console.log(id);
   //사용자 id 출력
 
-  return <div>Home page</div>;
+  return (
+    <div>
+      <LogoWrapper>
+        <Logo src="../public/logo/logo.png" alt="logo"></Logo>
+      </LogoWrapper>
+      <ContentWrapper>
+        <GridLayout>
+          <CalendarContainer>
+            <CalendarInnerContainer></CalendarInnerContainer>
+          </CalendarContainer>
+          <TODOContainer>
+            <TODOInnerContainer>
+              <ContainerTitle>ADD TODO</ContainerTitle>
+              <TODOInput placeholder="할 일을 추가하세요."></TODOInput>
+              <ButtonWrapper>
+                <AddButton>
+                  <AddButtonImg src="../public/Done.png"></AddButtonImg>
+                </AddButton>
+              </ButtonWrapper>
+            </TODOInnerContainer>
+          </TODOContainer>
+          <ListContainer>
+            <ListInnerContainer>
+              <ContainerTitle>MY TODO LIST</ContainerTitle>
+            </ListInnerContainer>
+          </ListContainer>
+        </GridLayout>
+      </ContentWrapper>
+    </div>
+  );
 };
 
 export default Home;
 
 // 좌상단 로고
 // 3개의 container
-// 구성
-// Container 1. 이전에 만든 달력 활용
-// Container 2. text area, 입력 버튼
-// Container 3. 할일 목록 표시, scroll 가능
+//
